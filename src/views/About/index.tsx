c5e3283b4f0e94d5d1afc8803639a54953b93b62
DayYoung <dayyoung@DayYoungdeMacBook-Pro.local>
2020-11-10 17:17:29 +0800

    feat: add title

--- src/views/About/index.tsx
+++ src/views/About/index.tsx
@@ -1,9 +1,13 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import { Helmet } from "react-helmet"
 
 class About extends Component {
   render() {
     return <div>
+      <Helmet>
+          <title>关于-合作伙伴</title>
+      </Helmet>
       <p>this is about page</p>
       <Link to="/">goto Home</Link>
     </div>
