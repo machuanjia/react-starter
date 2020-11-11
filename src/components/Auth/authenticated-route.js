677e3689c3164cd03e1c347a417eb660b89d8c52
DayYoung <dayyoung@DayYoungdeMacBook-Pro.local>
2020-11-11 10:39:43 +0800

    chore: format

--- src/components/Auth/authenticated-route.js
+++ src/components/Auth/authenticated-route.js
@@ -1,14 +1,17 @@
-import React from "react";
-import { Route, Redirect } from "react-router-dom";
+/** @format */
 
-export default ({ component: C, props: cProps, ...rest }) =>
-  <Route
-    {...rest}
-    render={props =>
-      cProps.isAuthenticated
-        ? <C {...props} {...cProps} />
-        : <Redirect
-            to={`/login?redirect=${props.location.pathname}${props.location
-              .search}`}
-          />}
-  />;
+import React from 'react'
+import {Route, Redirect} from 'react-router-dom'
+
+export default ({component: C, props: cProps, ...rest}) => (
+    <Route
+        {...rest}
+        render={props =>
+            cProps.isAuthenticated ? (
+                <C {...props} {...cProps} />
+            ) : (
+                <Redirect to={`/login?redirect=${props.location.pathname}${props.location.search}`} />
+            )
+        }
+    />
+)
